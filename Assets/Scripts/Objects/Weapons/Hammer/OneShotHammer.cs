85d40e5e9c8daac81bef83781e05fdf10e96035e
aurelenc <aurele.nicolas@epitech.eu>
2024-06-05 01:26:18 +0900

    Revert "Merge branch 'main' into Map"
    This reverts commit 68009518f3721ae4cad1836741a01521c3d3e091, reversing
    changes made to 25af8d659e2684e83cc695ef277e6ee641470fe7.

--- Assets/Scripts/Objects/Weapons/Hammer/OneShotHammer.cs
+++ Assets/Scripts/Objects/Weapons/Hammer/OneShotHammer.cs
@@ -5,31 +5,16 @@ using UnityEngine;
 public class OneShotHammer : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public Transform projectionOrigin;
-
     void Start()
     {
         
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            HitPlayer(other);
-        }
-    }
 
-    void HitPlayer(Collider player)
-    {
-        RagdollTrigger ragdollTrigger = player.GetComponent<RagdollTrigger>();
-        float projectionForce = this.GetComponentInParent<MeleeWeaponStats>().projectionForce;
-        ragdollTrigger.EnableRagdoll();
-        ragdollTrigger.ApplyForce(projectionForce, projectionOrigin.transform.right, ForceMode.Impulse);
-        ragdollTrigger.StartRecoveryTime();
-        GetComponentInParent<WeaponHolder>().Drop();
-        Destroy(transform.parent.gameObject, 0.5f);
-    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
