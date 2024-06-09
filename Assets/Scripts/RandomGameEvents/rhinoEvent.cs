fc766c5ddc936c68c0590ea061c6ea48887444ec
Erwan <erwan.simonetti@epitech.eu>
2024-06-09 09:35:50 +0900

    feat(rhino): started event

--- Assets/Scripts/RandomGameEvents/rhinoEvent.cs
+++ Assets/Scripts/RandomGameEvents/rhinoEvent.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class rhinoEvent : randomEvent
 {
+    public Vector3 spawnPoint = new Vector3(0,1,3);
+    public GameObject rhinoPrefab;
+    private GameObject rhinoCopy;
+
     protected override void CustomStartEvent()
     {
         Debug.Log("Rhino event start !");
+        rhinoCopy = Instantiate(rhinoPrefab, spawnPoint, Quaternion.identity);
     }
 
     protected override void CustomEndEvent()
     {
         Debug.Log("Rhino event stop !");
+        Destroy(rhinoCopy);
     }
 
     void Update()
