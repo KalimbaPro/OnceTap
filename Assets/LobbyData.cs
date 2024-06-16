6daafc7277bb7123faf01812e53477ba8551c56a
aurelenc <aurele.nicolas@epitech.eu>
2024-06-16 18:03:46 +0900

    Fix lobby spawn

---
+++ Assets/LobbyData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyData : MonoBehaviour
+{
+    public Transform SpawnPointCorner1;
+    public Transform SpawnPointCorner2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
